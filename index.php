1b18bf62978fce0d14e35fc36237c5d7226e8335
AndreVictor60 <andre.victor60@free.fr>
2017-12-12 20:54:34 +0100

    fini

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
-<?php
-  $nom = 'Andre';
-  echo $nom;
- ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Partie 1 Exercice 1</title>
+  </head>
+  <body>
+    <?php
+      $nom = 'Andre';
+      echo $nom;
+     ?>
+  </body>
+</html>
